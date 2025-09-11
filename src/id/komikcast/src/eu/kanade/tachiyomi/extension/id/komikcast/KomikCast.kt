2fe7aa459629a88ee36955907501e51fb47883c2
extmod <julptk8@gmail.com>
2025-09-11 23:47:57 +0700

    Update KomikCast.kt

--- src/id/komikcast/src/eu/kanade/tachiyomi/extension/id/komikcast/KomikCast.kt
+++ src/id/komikcast/src/eu/kanade/tachiyomi/extension/id/komikcast/KomikCast.kt
@@ -1,302 +1,184 @@
 package eu.kanade.tachiyomi.extension.id.komikcast
 
+import android.app.Application
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
-import java.text.SimpleDateFormat
+import eu.kanade.tachiyomi.util.asJsoup
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import java.util.Calendar
+import java.util.Locale
 
-class KomikCast : ParsedHttpSource() {
+class KomikCast : MangaThemesia("Komik Cast", "https://komikcast.li", "id", "/daftar-komik"), ConfigurableSource {
 
-    override val name = "Komik Cast"
-    override val baseUrl = "https://komikcast.com"
-    override val lang = "id"
-    override val supportsLatest = true
-    override val client: OkHttpClient = network.cloudflareClient
+    private val preferences = Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    
+    private val resizeCover = "https://wsrv.nl/?w=110&h=150&url="
 
-    override fun popularMangaRequest(page: Int): Request {
-        return GET("$baseUrl/daftar-komik/page/$page/?order=popular", headers)
-    }
+    override var baseUrl = preferences.getString("overrideBaseUrl", super.baseUrl)!!
 
-    override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl/komik/page/$page/", headers)
-    }
+    // Formerly "Komik Cast (WP Manga Stream)"
+    override val id = 972717448578983812
 
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = if (query.isNotBlank()) {
-            val url = HttpUrl.parse("$baseUrl/page/$page")!!.newBuilder()
-            val pattern = "\\s+".toRegex()
-            val q = query.replace(pattern, "+")
-            if (query.isNotEmpty()) {
-                url.addQueryParameter("s", q)
-            } else {
-                url.addQueryParameter("s", "")
-            }
-            url.toString()
-        } else {
-            val url = HttpUrl.parse("$baseUrl/daftar-komik/page/$page")!!.newBuilder()
-            var orderBy = ""
-            (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
-                when (filter) {
-                    is Status -> url.addQueryParameter("status", arrayOf("", "ongoing", "completed")[filter.state])
-                    is GenreList -> {
-                        val genreInclude = mutableListOf<String>()
-                        filter.state.forEach {
-                            if (it.state == 1) {
-                                genreInclude.add(it.id)
-                            }
-                        }
-                        if (genreInclude.isNotEmpty()) {
-                            genreInclude.forEach { genre ->
-                                url.addQueryParameter("genre[]", genre)
-                            }
-                        }
-                    }
-                    is SortBy -> {
-                        orderBy = filter.toUriPart()
-                        url.addQueryParameter("order", orderBy)
-                    }
-                }
-            }
-            url.toString()
-        }
-        return GET(url, headers)
-    }
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(3)
+        .build()
 
-    override fun popularMangaSelector() = "div.bs"
-    override fun latestUpdatesSelector() = popularMangaSelector()
-    override fun searchMangaSelector() = popularMangaSelector()
-
-    override fun popularMangaFromElement(element: Element): SManga {
-        val manga = SManga.create()
-        manga.thumbnail_url = element.select("div.limit img").attr("src")
-        element.select("div.bigor > a").first().let {
-            manga.setUrlWithoutDomain(it.attr("href"))
-            manga.title = it.attr("title")
-        }
-        return manga
-    }
-
-    override fun searchMangaFromElement(element: Element): SManga = popularMangaFromElement(element)
-    override fun latestUpdatesFromElement(element: Element): SManga = popularMangaFromElement(element)
-
-    override fun popularMangaNextPageSelector() = "a.next.page-numbers"
-    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
-    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
-
-    override fun mangaDetailsParse(document: Document): SManga {
-        val infoElement = document.select("div.spe").first()
-        val sepName = infoElement.select(".spe > span:nth-child(4)").last()
-        val manga = SManga.create()
-        manga.author = sepName.ownText()
-        manga.artist = sepName.ownText()
-        val genres = mutableListOf<String>()
-        infoElement.select(".spe > span:nth-child(1) > a").forEach { element ->
-            val genre = element.text()
-            genres.add(genre)
-        }
-        manga.genre = genres.joinToString(", ")
-        manga.status = parseStatus(infoElement.select(".spe > span:nth-child(2)").text())
-        manga.description = document.select("div[^itemprop]").last().text()
-        manga.thumbnail_url = document.select(".thumb > img:nth-child(1)").attr("src")
-
-        return manga
-    }
-
-    private fun parseStatus(element: String): Int = when {
-
-        element.toLowerCase().contains("ongoing") -> SManga.ONGOING
-        element.toLowerCase().contains("completed") -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
-    }
-
-    override fun chapterListSelector() = "div.cl ul li"
-
-    override fun chapterFromElement(element: Element): SChapter {
-        val urlElement = element.select("a").first()
-        val timeElement = element.select("span.rightoff").first()
-        val chapter = SChapter.create()
-        chapter.setUrlWithoutDomain(urlElement.attr("href"))
-        chapter.name = urlElement.text()
-        chapter.date_upload = parseChapterDate(timeElement.text())
-        return chapter
-    }
-
-    private fun parseChapterDate(date: String): Long {
-        val value = date.split(' ')[0].toInt()
-
-        return when {
-            "mins" in date -> Calendar.getInstance().apply {
-                add(Calendar.MINUTE, value * -1)
-            }.timeInMillis
-            "hours" in date -> Calendar.getInstance().apply {
-                add(Calendar.HOUR_OF_DAY, value * -1)
-            }.timeInMillis
-            "days" in date -> Calendar.getInstance().apply {
-                add(Calendar.DATE, value * -1)
-            }.timeInMillis
-            "weeks" in date -> Calendar.getInstance().apply {
-                add(Calendar.DATE, value * 7 * -1)
-            }.timeInMillis
-            "months" in date -> Calendar.getInstance().apply {
-                add(Calendar.MONTH, value * -1)
-            }.timeInMillis
-            "years" in date -> Calendar.getInstance().apply {
-                add(Calendar.YEAR, value * -1)
-            }.timeInMillis
-            "min" in date -> Calendar.getInstance().apply {
-                add(Calendar.MINUTE, value * -1)
-            }.timeInMillis
-            "hour" in date -> Calendar.getInstance().apply {
-                add(Calendar.HOUR_OF_DAY, value * -1)
-            }.timeInMillis
-            "day" in date -> Calendar.getInstance().apply {
-                add(Calendar.DATE, value * -1)
-            }.timeInMillis
-            "week" in date -> Calendar.getInstance().apply {
-                add(Calendar.DATE, value * 7 * -1)
-            }.timeInMillis
-            "month" in date -> Calendar.getInstance().apply {
-                add(Calendar.MONTH, value * -1)
-            }.timeInMillis
-            "year" in date -> Calendar.getInstance().apply {
-                add(Calendar.YEAR, value * -1)
-            }.timeInMillis
-            else -> {
-                return 0
-            }
-        }
-    }
-
-    override fun prepareNewChapter(chapter: SChapter, manga: SManga) {
-        val basic = Regex("""Chapter\s([0-9]+)""")
-        when {
-            basic.containsMatchIn(chapter.name) -> {
-                basic.find(chapter.name)?.let {
-                    chapter.chapter_number = it.groups[1]?.value!!.toFloat()
-                }
-            }
-        }
-    }
-
-    override fun pageListParse(document: Document): List<Page> {
-        val pages = mutableListOf<Page>()
-        var i = 0
-        document.select("div#readerarea img").forEach { element ->
-            val url = element.attr("src")
-            i++
-            if (url.length > 0) {
-                pages.add(Page(i, "", url))
-            }
-        }
-        return pages
-    }
-
-    override fun imageUrlParse(document: Document) = ""
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder()
+        .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+        .add("Accept-language", "en-US,en;q=0.9,id;q=0.8")
 
     override fun imageRequest(page: Page): Request {
-        var headers = Headers.Builder()
-        headers.apply {
-            add("Referer", baseUrl)
-            add("User-Agent", "Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; LGMS323 Build/KOT49I.MS32310c) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/76.0.3809.100 Mobile Safari/537.36")
-        }
+        val newHeaders = headersBuilder()
+            .set("Accept", "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8")
+            .set("Referer", "$baseUrl/")
+            .build()
 
-        if (page.imageUrl!!.contains("i0.wp.com")) {
-            headers.apply {
-                add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
-            }
-        }
-        return GET(page.imageUrl!!, headers.build())
+        return GET(page.imageUrl!!, newHeaders)
     }
 
-    private class SortBy : UriPartFilter("Sort by", arrayOf(
-            Pair("Default", ""),
-            Pair("A-Z", "title"),
-            Pair("Z-A", "titlereverse"),
-            Pair("Latest Update", "update"),
-            Pair("Latest Added", "latest"),
-            Pair("Popular", "popular")
-    ))
+    override fun searchMangaSelector() = "div.list-update_item"
 
-    private class Status : UriPartFilter("Status", arrayOf(
-            Pair("All", ""),
-            Pair("Ongoing", "Ongoing"),
-            Pair("Completed", "Completed")
-    ))
+        override fun searchMangaFromElement(element: Element): SManga {
+    return super.searchMangaFromElement(element).apply {
+        thumbnail_url = "$resizeCover$thumbnail_url"
+        title = element.selectFirst("h3.title")!!.ownText()
+    }
+}
 
-    private class Genre(name: String, val id: String = name) : Filter.TriState(name)
-    private class GenreList(genres: List<Genre>) : Filter.Group<Genre>("Genres", genres)
+    override val seriesDetailsSelector = "div.komik_info:has(.komik_info-content)"
+    override val seriesTitleSelector = "h1.komik_info-content-body-title"
+    override val seriesDescriptionSelector = ".komik_info-description-sinopsis"
+    override val seriesAltNameSelector = ".komik_info-content-native"
+    override val seriesGenreSelector = ".komik_info-content-genre a"
+    override val seriesThumbnailSelector = ".komik_info-content-thumbnail img"
+    override val seriesStatusSelector = ".komik_info-content-info:contains(Status)"
 
-    override fun getFilterList() = FilterList(
-            Filter.Header("NOTE: Ignored if using text search!"),
-            Filter.Separator(),
-            SortBy(),
-            Filter.Separator(),
-            Status(),
-            Filter.Separator(),
-            GenreList(getGenreList())
-    )
+    override fun mangaDetailsParse(document: Document) = SManga.create().apply {
+        document.selectFirst(seriesDetailsSelector)?.let { seriesDetails ->
+            title = seriesDetails.selectFirst(seriesTitleSelector)?.text()
+                ?.replace("bahasa indonesia", "", ignoreCase = true)?.trim().orEmpty()
+            artist = seriesDetails.selectFirst(seriesArtistSelector)?.ownText().removeEmptyPlaceholder()
+            author = seriesDetails.selectFirst(seriesAuthorSelector)?.ownText().removeEmptyPlaceholder()
+            description = seriesDetails.select(seriesDescriptionSelector).joinToString("\n") { it.text() }.trim()
+            // Add alternative name to manga description
+            val altName = seriesDetails.selectFirst(seriesAltNameSelector)?.ownText().takeIf { it.isNullOrBlank().not() }
+            altName?.let {
+                description = "$description\n\n$altNamePrefix$altName".trim()
+            }
+            val genres = seriesDetails.select(seriesGenreSelector).map { it.text() }.toMutableList()
+            // Add series type (manga/manhwa/manhua/other) to genre
+            seriesDetails.selectFirst(seriesTypeSelector)?.ownText().takeIf { it.isNullOrBlank().not() }?.let { genres.add(it) }
+            genre = genres.map { genre ->
+                genre.lowercase(Locale.forLanguageTag(lang)).replaceFirstChar { char ->
+                    if (char.isLowerCase()) {
+                        char.titlecase(Locale.forLanguageTag(lang))
+                    } else {
+                        char.toString()
+                    }
+                }
+            }
+                .joinToString { it.trim() }
 
-    private fun getGenreList() = listOf(
-            Genre("4-Koma", "4-koma"),
-            Genre("Action", "action"),
-            Genre("Adventure", "adventure"),
-            Genre("Comedy", "comedy"),
-            Genre("Cooking", "cooking"),
-            Genre("Demons", "demons"),
-            Genre("Drama", "drama"),
-            Genre("Ecchi", "ecchi"),
-            Genre("Fantasy", "fantasy"),
-            Genre("Game", "game"),
-            Genre("Gender Bender", "gender-bender"),
-            Genre("Harem", "harem"),
-            Genre("Historical", "historical"),
-            Genre("Horror", "horror"),
-            Genre("Isekai ", "isekai"),
-            Genre("Josei", "josei"),
-            Genre("Magic", "magic"),
-            Genre("Martial Arts", "martial-arts"),
-            Genre("Mature", "mature"),
-            Genre("Mecha", "mecha"),
-            Genre("Medical", "medical"),
-            Genre("Military", "military"),
-            Genre("Mistery", "mistery"),
-            Genre("Music", "music"),
-            Genre("Mystery", "mystery"),
-            Genre("Psychological", "psychological"),
-            Genre("Romance", "romance"),
-            Genre("School", "school"),
-            Genre("School Life", "school-life"),
-            Genre("Sci-Fi", "sci-fi"),
-            Genre("Seinen", "seinen"),
-            Genre("Shoujo", "shoujo"),
-            Genre("Shoujo Ai", "shoujo-ai"),
-            Genre("Shounen", "shounen"),
-            Genre("Shounen Ai", "shounen-ai"),
-            Genre("Slice of Life", "slice-of-life"),
-            Genre("Sports", "sports"),
-            Genre("Super Power", "super-power"),
-            Genre("Supernatural", "supernatural"),
-            Genre("Thriller", "thriller"),
-            Genre("Tragedy", "tragedy"),
-            Genre("Vampire", "vampire"),
-            Genre("Webtoons", "webtoons"),
-            Genre("Yuri", "yuri")
-    )
+            status = seriesDetails.selectFirst(seriesStatusSelector)?.text().parseStatus()
+            thumbnail_url = resizeCover + seriesDetails.select(seriesThumbnailSelector).imgAttr()
+        }
+    }
 
-    private open class UriPartFilter(displayName: String, val vals: Array<Pair<String, String>>) :
-            Filter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
-        fun toUriPart() = vals[state].second
+    override fun chapterListSelector() = "div.komik_info-chapters li"
+
+    override fun chapterFromElement(element: Element) = SChapter.create().apply {
+        val urlElements = element.select("a")
+        setUrlWithoutDomain(urlElements.attr("href"))
+        name = element.select(".chapter-link-item").text()
+        date_upload = parseChapterDate2(element.select(".chapter-link-time").text())
+    }
+
+    private fun parseChapterDate2(date: String): Long {
+        return if (date.endsWith("ago")) {
+            val value = date.split(' ')[0].toInt()
+            when {
+                "min" in date -> Calendar.getInstance().apply {
+                    add(Calendar.MINUTE, -value)
+                }.timeInMillis
+                "hour" in date -> Calendar.getInstance().apply {
+                    add(Calendar.HOUR_OF_DAY, -value)
+                }.timeInMillis
+                "day" in date -> Calendar.getInstance().apply {
+                    add(Calendar.DATE, -value)
+                }.timeInMillis
+                "week" in date -> Calendar.getInstance().apply {
+                    add(Calendar.DATE, -value * 7)
+                }.timeInMillis
+                "month" in date -> Calendar.getInstance().apply {
+                    add(Calendar.MONTH, -value)
+                }.timeInMillis
+                "year" in date -> Calendar.getInstance().apply {
+                    add(Calendar.YEAR, -value)
+                }.timeInMillis
+                else -> {
+                    0L
+                }
+            }
+        } else {
+            try {
+                dateFormat.parse(date)?.time ?: 0
+            } catch (_: Exception) {
+                0L
+            }
+        }
+    }
+
+    override fun pageListParse(response: okhttp3.Response): List<Page> {
+    val doc = response.asJsoup()
+    val service = preferences.getString("resize_service_url", "")
+
+    return doc.select("div#chapter_body .main-reading-area img.size-full").mapIndexed { i, img ->
+        val src = img.imgAttr().trim()
+        val finalUrl = "$service$src"
+        Page(i, "", finalUrl)
+    }
+}
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val resizeServicePref = EditTextPreference(screen.context).apply {
+            key = "resize_service_url"
+            title = "Resize Service URL (Pages)"
+            summary = "Masukkan URL layanan resize gambar untuk halaman (page list)."
+            setDefaultValue(null)
+            dialogTitle = "Resize Service URL"
+        }
+        screen.addPreference(resizeServicePref)
+
+        val baseUrlPref = EditTextPreference(screen.context).apply {
+            key = "overrideBaseUrl"
+            title = "Ubah Domain"
+            summary = "Update domain untuk ekstensi ini"
+            setDefaultValue(baseUrl)
+            dialogTitle = "Update domain untuk ekstensi ini"
+            dialogMessage = "Original: $baseUrl"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val newUrl = newValue as String
+                baseUrl = newUrl
+                preferences.edit().putString("overrideBaseUrl", newUrl).apply()
+                summary = "Current domain: $newUrl"
+                true
+            }
+        }
+        screen.addPreference(baseUrlPref)
     }
 }
