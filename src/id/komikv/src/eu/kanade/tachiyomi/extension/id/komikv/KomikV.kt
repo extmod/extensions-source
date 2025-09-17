c706a1b76233de79cfea1c81a4eefdeecbde7812
extmod <julptk8@gmail.com>
2025-09-17 16:57:01 +0700

    Update KomikV.kt

--- src/id/komikv/src/eu/kanade/tachiyomi/extension/id/komikv/KomikV.kt
+++ src/id/komikv/src/eu/kanade/tachiyomi/extension/id/komikv/KomikV.kt
@@ -17,6 +17,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.net.URLEncoder
+import java.net.URLDecoder
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
