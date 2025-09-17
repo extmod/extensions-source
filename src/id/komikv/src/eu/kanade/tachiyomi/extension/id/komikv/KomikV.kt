b1c0dfbac20eddfe83375b6b4765c06be2bf059c
extmod <julptk8@gmail.com>
2025-09-17 23:17:07 +0700

    Update KomikV.kt

--- src/id/komikv/src/eu/kanade/tachiyomi/extension/id/komikv/KomikV.kt
+++ src/id/komikv/src/eu/kanade/tachiyomi/extension/id/komikv/KomikV.kt
@@ -9,6 +9,7 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
