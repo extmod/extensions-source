2812c0964963a9ce597a3204c04b5a41046b62f6
extmod <julptk8@gmail.com>
2025-09-17 16:09:19 +0700

    Update AV.kt

--- src/id/av/src/eu/kanade/tachiyomi/extension/id/av/AV.kt
+++ src/id/av/src/eu/kanade/tachiyomi/extension/id/av/AV.kt
@@ -8,6 +8,7 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.nodes.Document
