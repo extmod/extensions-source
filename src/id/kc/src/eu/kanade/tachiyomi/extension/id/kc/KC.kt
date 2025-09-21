69a8d385b4c5ca478f4c135ed323923d0da2d946
extmod <julptk8@gmail.com>
2025-09-21 17:19:35 +0700

    Update KC.kt

--- src/id/kc/src/eu/kanade/tachiyomi/extension/id/kc/KC.kt
+++ src/id/kc/src/eu/kanade/tachiyomi/extension/id/kc/KC.kt
@@ -4,6 +4,7 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
