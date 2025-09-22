c7270f426147dc3e13bfb9bb0a1db69aec2be327
extmod <julptk8@gmail.com>
2025-09-22 09:36:44 +0700

    Update KomikV.kt

--- src/id/komikv/src/eu/kanade/tachiyomi/extension/id/komikv/KomikV.kt
+++ src/id/komikv/src/eu/kanade/tachiyomi/extension/id/komikv/KomikV.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.Page
