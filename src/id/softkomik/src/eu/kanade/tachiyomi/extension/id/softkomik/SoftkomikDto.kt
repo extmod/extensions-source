bceaa69f7dd36898055d6d5ea694288d6ae958ae
extmod <julptk8@gmail.com>
2026-03-27 18:06:44 +0700

    Update SoftkomikDto.kt

--- src/id/softkomik/src/eu/kanade/tachiyomi/extension/id/softkomik/SoftkomikDto.kt
+++ src/id/softkomik/src/eu/kanade/tachiyomi/extension/id/softkomik/SoftkomikDto.kt
@@ -1,35 +1,32 @@
 package eu.kanade.tachiyomi.extension.id.softkomik
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class LibDataDto(
-    val page: Int,
-    val maxPage: Int,
     val data: List<MangaDto>,
+    val maxPage: Int,
+    val page: Int,
 )
 
 @Serializable
 data class MangaDto(
+    val gambar: String,
     val title: String,
+    val title_slug: String,
     val status: String? = null,
     val type: String? = null,
-    val gambar: String,
-    val title_slug: String,
 )
 
 @Serializable
 data class MangaDetailsDto(
+    val gambar: String,
     val title: String,
-    val title_alt: String? = null,
-    val sinopsis: String? = null,
     val author: String? = null,
+    val Genre: List<String>? = emptyList(),
+    val sinopsis: String? = null,
     val status: String? = null,
     val type: String? = null,
-    val gambar: String,
-    val updated_at: String? = null,
-    val Genre: List<String>? = emptyList(),
 )
 
 @Serializable
@@ -38,49 +35,18 @@ data class ChapterDto(
 )
 
 @Serializable
-data class ChapterListDto(
-    val title: String = "",
-    val chapter: List<ChapterDto>,
-    val startChapter: List<ChapterDto> = emptyList(),
-    val newChapter: List<ChapterDto> = emptyList(),
-)
-
-@Serializable
-data class NextDataDto(val buildId: String = "")
-
-@Serializable
-data class ChapterPageImagesDto(val imageSrc: List<String>)
-
-@Serializable
-data class LibraryDto(val pageProps: LibraryPagePropsDto)
-
-@Serializable
-data class LibraryPagePropsDto(val libData: LibDataDto)
-
-@Serializable
-data class DetailsDto(val pageProps: DetailsPagePropsDto)
-
-@Serializable
-data class DetailsPagePropsDto(val data: MangaDetailsDto)
-
-@Serializable
-data class ChapterPageDto(val pageProps: ChapterPageDataWrapperDto)
-
-@Serializable
-data class ChapterPageDataWrapperDto(val data: ChapterPageContentDto)
-
-@Serializable
-data class ChapterPageContentDto(val data: ChapterDataDto)
-
-@Serializable
-data class ChapterDataDto(
-    @SerialName("_id") val id: String,
+data class ChapterPageDataDto(
     val imageSrc: List<String> = emptyList(),
 )
 
 @Serializable
+data class ChapterListDto(
+    val chapter: List<ChapterDto>,
+)
+
+@Serializable
 data class SessionDto(
-    val token: String,
+    val ex: Long,
     val sign: String,
-    val ex: Long = 0L,
+    val token: String,
 )
