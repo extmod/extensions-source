180adc9ff934fba32f4297ac62e7ca811d15588f
extmod <julptk8@gmail.com>
2026-04-11 21:00:20 +0700

    Update KomikCastCC.kt

--- src/id/komikcastcc/src/eu/kanade/tachiyomi/extension/id/komikcastcc/KomikCastCC.kt
+++ src/id/komikcastcc/src/eu/kanade/tachiyomi/extension/id/komikcastcc/KomikCastCC.kt
@@ -9,6 +9,7 @@ import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
