62feb20837b3a3dd8b9b07de69b0b4a55e572812
extmod <julptk8@gmail.com>
2026-04-13 23:09:07 +0700

    Update SoftkomikDto.kt

--- src/id/softkomik/src/eu/kanade/tachiyomi/extension/id/softkomik/SoftkomikDto.kt
+++ src/id/softkomik/src/eu/kanade/tachiyomi/extension/id/softkomik/SoftkomikDto.kt
@@ -3,40 +3,48 @@ package eu.kanade.tachiyomi.extension.id.softkomik
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class SessionDto(
+    val token: String,
+    val sign: String,
+    val ex: Long,
+)
+
+@Serializable
 data class LibDataDto(
     val data: List<MangaDto>,
-    val maxPage: Int,
     val page: Int,
+    val maxPage: Int,
 )
 
 @Serializable
 data class MangaDto(
-    val gambar: String,
     val title: String,
     val title_slug: String,
+    val gambar: String,
     val status: String? = null,
     val type: String? = null,
+    val latest_chapter: String? = null,
 )
 
 @Serializable
 data class MangaDetailsDto(
-    val gambar: String,
     val title: String,
     val author: String? = null,
-    val Genre: List<String>? = emptyList(),
     val sinopsis: String? = null,
+    val Genre: List<String>? = emptyList(),
     val status: String? = null,
     val type: String? = null,
+    val gambar: String,
+)
+
+@Serializable
+data class ChapterListDto(
+    val chapter: List<ChapterDto>,
 )
 
 @Serializable
 data class ChapterDto(
     val chapter: String,
-)
-
-@Serializable
-data class ChapterPageImagesDto(
-    val imageSrc: List<String>,
 )
 
 @Serializable
@@ -47,19 +55,6 @@ data class ChapterPageDataDto(
 )
 
 @Serializable
-data class ChapterListDto(
-    val chapter: List<ChapterDto>,
-)
-
-@Serializable
-data class SessionDto(
-    val ex: Long,
-    val sign: String,
-    val token: String,
-)
-
-@Serializable
-class BearerTokenDto(
-    val token: String,
-    val ex: Long,
+data class ChapterPageImagesDto(
+    val imageSrc: List<String>,
 )
